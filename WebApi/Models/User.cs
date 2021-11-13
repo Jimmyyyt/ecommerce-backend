c1fcdd0a9839d814562d49ff43f8ffaeb729a41b
Jimmy Torstenson <thorstensonj@gmail.com>
2021-11-13 19:04:21 +0100

    update

--- WebApi/Models/User.cs
+++ WebApi/Models/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,15 +14,19 @@ namespace WebApi.Models
         public int Id { get; set; }
 
         [Required]
+        [Column(TypeName = "nvarchar(50)")]
         public string FirstName { get; set; }
         
         [Required]
+        [Column(TypeName = "nvarchar(50)")]
         public string LastName { get; set; }
         
         [Required]
+        [Column(TypeName = "varchar(100)")]
         public string Email { get; set; }
         
         [Required]
+        [Column(TypeName = "varchar(100)")]
         public string Password { get; set; }
     }
 }
